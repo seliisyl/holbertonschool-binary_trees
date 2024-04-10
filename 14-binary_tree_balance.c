2aba1fb73b4d46d03302099157497c9815a4a6d7
seliisyl <8859@holbertonstudents.com>
2024-04-10 10:00:50 +0000

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,17 +18,23 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 	return (1 + (height_left > height_right ? height_left : height_right));
-			}
+}
 
-			/**
-			 * binary_tree_balance - Calcule le facteur d'équilibre d'un arbre binair
-			 * @tree: Un pointeur vers le node racine de l'arbre
-			 *
-			 * Return: Le facteur d'équilibre de l'arbre, ou 0 si l'arbre est NUL
-			 */
-			int binary_tree_balance(const binary_tree_t *tree)
-			{
-			if (tree == NULL)
-			return (0);
-			return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-					}
+/**
+ * binary_tree_balance - Calcule le facteur d'équilibre d'un arbre binair
+ * @tree: Un pointeur vers le node racine de l'arbre
+ *
+ * Return: Le facteur d'équilibre de l'arbre, ou 0 si l'arbre est NUL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance_left, balance_right;
+
+	if (tree == NULL)
+		return (0);
+
+	balance_left = binary_tree_height(tree->left);
+	balance_right = binary_tree_height(tree->right);
+
+	return (balance_left - balance_right);
+}
